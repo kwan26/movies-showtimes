955d80e322f78c675c1246a180caa669ba024dd2
Tiago Mestre <tiago.mestre@log.pt>
2013-11-26 17:43:15 +0000

    alteracoes de codigo

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,10 @@
  * Namespace the application
  * @namespace SB
  */
-if (!MS) var MS = {};
-else if (MS && typeof (MS) != "object") throw new Error("MS is not an Object type");
+if (!MS)
+    var MS = {};
+else if (MS && typeof (MS) != "object")
+    throw new Error("MS is not an Object type");
 
 /**
  * Constructs Main objects
@@ -12,11 +14,11 @@ else if (MS && typeof (MS) != "object") throw new Error("MS is not an Object typ
  * @namespace SB
  */
 
-MS.Main = (function ($) {
-     // PRIVATE ATTRIBUTES. ONLY PRIVELEGED METHODS MAY VIEW/EDIT/INVOKE
+MS.Main = (function($) {
+    // PRIVATE ATTRIBUTES. ONLY PRIVELEGED METHODS MAY VIEW/EDIT/INVOKE
     var _that = {};
     var _site = "http://showtimes.tiagomestre.pt";
-    
+
 
     // PRIVATE MEMBERS. ONLY PRIVELEGED METHODS MAY VIEW/EDIT/INVOKE.
 
@@ -25,340 +27,250 @@ MS.Main = (function ($) {
         // PUBLIC ATTRIBUTES ANYONE MAY READ/WRITE.
         NAME: "Application initialize module",
         VERSION: 1.0,
-
         // PUBLIC MEMBERS ANYONE MAY READ/WRITE. (MAY BE OVERRIDEN).
         // PUBLIC METHOD THAT INITIALIZES MAIN APP.
-        init: function () {
+        init: function() {
             _that = this;
-            _that.saved_tid  = [];
+            _that.saved_tid = [];
             this.loadInits();
-            
+
         },
-        
-         /**
-        * Run all the init methods
-        */
-        loadInits: function () {
+        /**
+         * Run all the init methods
+         */
+        loadInits: function() {
             for (var func in this) {
                 if (typeof this[func] == 'function' && func.substr(0, 4) == 'init' && func != 'init') {
                     this[func]();
                 }
             }
         },
-        
-        inditCheckFavorites:function(){
-            
-             
-           var  info = [];
-             info[0] = 'f9c3071ea95b333';
-             info[1] = '60e3dd0bed2523f6';
-             
-               var test =   _that.initgetOptions();
-            
-            
-             $.ajax({
-                url: _site+"/api/v2/movies/info/favourites",
-                data: {info:test.options},
+        inditCheckFavorites: function() {
+
+
+            var info = [];
+            info[0] = 'f9c3071ea95b333';
+            info[1] = '60e3dd0bed2523f6';
+
+            var test = _that.initgetOptions();
+
+
+            $.ajax({
+                url: _site + "/api/v2/movies/info/favourites",
+                data: {info: test.options},
                 type: 'GET',
                 dataType: 'json', // added data type
-                beforeSend:function(){
+                beforeSend: function() {
                     $("#loading").show();
                 },
                 success: function(res) {
-                 
-                          $("#loading").hide();
+
+                    $("#loading").hide();
                 },
-                error:function(error){
-                    
+                error: function(error) {
+
                 }
             });
-            
+
         },
-        
-        initCallWebServiceTheates:function(){
-            
-            
+        initCallWebServiceTheates: function() {
+
+
             var elOptions = $('.options'),
-                 elCheck = $('.addhere');
-         
-            if(!elOptions.length) return false;
+                    elCheck = $('.addhere');
+
+            if (!elOptions.length)
+                return false;
             elCheck.html();
-            
+
             $.ajax({
-               // url: "http://movies.log.local/api/v1/movies/loures?apiKey=debugkey",
-                 url: _site+"/api/v1/movies/theatres",
-              
+                // url: "http://movies.log.local/api/v1/movies/loures?apiKey=debugkey",
+                url: _site + "/api/v1/movies/theatres",
                 type: 'GET',
                 dataType: 'json', // added data type
-                beforeSend:function(){
+                beforeSend: function() {
                     $("#loading").show();
                 },
                 success: function(res) {
-                     var jsonResponse = res;
-                        $("#loading").hide();
-                     
-                      for(var movies in jsonResponse){
-                     
-                          elCheck.append(" <label class='checkbox'>"
-                                  +"<input type='checkbox' name='theatres' value='"+jsonResponse[movies].tid+"'class='theatres'>"
-                                  +jsonResponse[movies].sala
-                                  +"</label>");
-                          
-                          //          <label class="checkbox">
-                           // <input type="checkbox" name="theatres"> Check me out
-                          //</label>
-                      }
-                      _that.ingetOptions();
-                     
-                       
+                    var jsonResponse = res;
+                    $("#loading").hide();
+
+                    for (var movies in jsonResponse) {
+
+                        elCheck.append(" <label class='checkbox'>"
+                                + "<input type='checkbox' name='theatres' value='" + jsonResponse[movies].tid + "'class='theatres'>"
+                                + jsonResponse[movies].sala
+                                + "</label>");
+
+                        //          <label class="checkbox">
+                        // <input type="checkbox" name="theatres"> Check me out
+                        //</label>
+                    }
+                    _that.ingetOptions();
+
+
                 },
-                error:function(e){
-                    
+                error: function(e) {
+
                 }
-             });
-            
+            });
+
         },
-        
-        
-        initSaveOptions:function(){
-          
+        initSaveOptions: function() {
+
             var btn = $('.btn'),
-                 theatres = $('.theatres');
-         
-            btn.on('click',function(){
-               
-               var allVals = [];
-               
-               
+                    theatres = $('.theatres');
+
+            btn.on('click', function() {
+
+                var allVals = [];
+
+
                 $('.addhere :checked').each(function() {
-                  allVals.push($(this).val());
-                  
+                    allVals.push($(this).val());
+
                 });
-                
-                 chrome.storage.local.set({
-                        'options': allVals
+
+                chrome.storage.local.set({
+                    'options': allVals
                 });
-                
-                if(!allVals.length) console.log("nop");
+
+                if (!allVals.length)
+                    console.log("nop");
                 window.location = '';
-                
+
                 return false;
             });
         },
-        
-        ingetOptions:function(){
-      
-     
+        ingetOptions: function() {
+
+
             chrome.storage.local.get('options', function(data) {
-                
-                
+
+
                 var elOptions = $('.options');
-                
+
                 _that.saved_tid = data;
-               
-               
-               if(!elOptions.length) return false;
-               
+
+
+                if (!elOptions.length)
+                    return false;
+
                 $('input[name=theatres]').each(function() {
-                    
+
                     var el = this;
-                 
-                 
-                    for(var i = 0; i < data.options.length; i++){
-                   
-                        if($(el).val() == data.options[i]){
+
+
+                    for (var i = 0; i < data.options.length; i++) {
+
+                        if ($(el).val() == data.options[i]) {
                             $(el).prop('checked', true);
                         }
                     }
-                    
+
                 });
-           
-                
+
+
             });
-            
+
         },
-        
-        initOpen:function(){
-        
-            
+        initOpen: function() {
+
+
             var open = $('.open');
-            
-            if(!open.length) return false;
-            
-            open.click(function(){
-                
+
+            if (!open.length)
+                return false;
+
+            open.click(function() {
+
                 chrome.tabs.create({url: "options.html"});
-               return false; 
+                return false;
             });
         },
-        
-        
-        initCallWebService:function(){
-            
+        initCallWebService: function() {
+
             var elNoService = $("#feed-empty"),
-                 elOptions = $('.options');
-         
-            if(elOptions.length) return false;
-            
-            
+                    elOptions = $('.options');
+
+            if (elOptions.length)
+                return false;
+
+
             elNoService.hide();
-            
-              var  info = [];
+
+            var info = [];
             // info[0] = 'f9c3071ea95b333';
             // info[1] = '60e3dd0bed2523f6';
-             
-             
-              chrome.storage.local.get('options', function(data) {
-                  
-                  info  = data.options;
-                  
-                  if(!info.length){
-                   
-                   
-                      return false;
-                  }
-                  
-                     $(".page-header").hide();
-                  
-                  
-                  $.ajax({
-               // url: "http://movies.log.local/api/v1/movies/loures?apiKey=debugkey",
-                 url: "http://showtimes.tiagomestre.pt/api/v2/movies/info/favourites",
-                 data: {info:info},
-                type: 'GET',
-                dataType: 'json', // added data type
-                beforeSend:function(){
-                    
-                    $("#loading").show();
-                },
-                success: function(res) {
-                
-                    $("#divSelect").show();
-            
-             $('#selectSala').bind('change', function () {
-                         
-                            var url = $(this).val(); // get selected value
-                            
-                             $('html,body').animate({scrollTop: $("#"+url).offset().top},'slow');
-                        });
-                 $("#loading").hide();
-                    var jsonResponse = res;
-                    
-                    
-                       var tmpl = $('#simple').html();
-                      
-                      
-                        
-                       
-                          $("#sessions").html(Mustache.render(tmpl,jsonResponse ));
-                          
-                          console.log(jsonResponse);
-                          return;
-                        
-                      var res_trailer;  
-                      var cinemas =[]; 
-                      var count = 1;
-                      var rating = "";
-                         cinemas["data"] = [];
-                      cinemas["data"]["cinemas"] = []; 
-                      var select_jump = " <option value=''>Escolha a sala</option>";
-                       var a= 0;
-                      for(var point1 in jsonResponse.data){
-                          var item = jsonResponse.data[point1];
-                       
-                        /* var data =   {
-                      images: [
-                          { src: "http://www.fpoimg.com/20x20" },
-                          { src: "http://www.fpoimg.com/30x30" },
-                          { src: "http://www.fpoimg.com/40x40" }
-                      ],
-                      anEmptyArray: []};*/
-                     
-                          $('#selectSala').append('<option value="item-'+count+'">'+point1+'</option>');
-                          
 
-                          $("#movies").append("<h4><span class='label label-danger'><a id='item-"+count+"'>"+point1+"</a></span></h4>");
-                          for(var moviesshows in item){
-                              
-                              if(item[moviesshows].movie.trailer.length){
-                                   res_trailer = "<a href='"+item[moviesshows].movie.trailer+"' target='_blank' class='btn btn-primary btnNext'>Trailer</a> " 
-                                   res_trailer +="<a href='http://www.imdb.com/title/"+item[moviesshows].movie.imdb+"/' target='_blank' class='btn btn-primary btnNext'>Imdb</a>" 
-                                   res_trailer +="  <a href='#' class='btn btn-primary btnNext'>UP</a><h4>" 
-                               }
-                               
-                               if(item[moviesshows].movie.poster.length){
-                                   test = item[moviesshows].movie.poster;
-                               }else{
-                                   test = "img/claquete_318-1578.jpg";
-                                  
-                               }
-                                if(item[moviesshows].movie.imdb_rating != "N/A"){
-                                    rating =  "<span class='glyphicon glyphicon-star'></span> "+item[moviesshows].movie.imdb_rating
-                                }else{
-                                    rating = "";
-                                }
-                               
-                               
-                              $("#movies").append("<div class='row'>"
-                                    +""
-                                    +"<div class='col-md-2 col-sm-3 text-center'>"
-                                     +"<img  src='"+test+"' style='width:100px;height:159px' class='img-rounded'></a>"
-                                     +"<div class='info'><h3>"+item[moviesshows].movie.name+"</h3>"
-                                     +rating
-                              +"<h4><span class='label label-danger' style='margin-bottom:10px;'>"+point1+"</span></h4> "
-                                          +"<small style='font-family:courier,\'new courier\';' class='btn btn-primary btnNext'>"+item[moviesshows].movie.time+"</small>"
-                                       
-                                    +"</h4>"
-                            
-                                   +"<h4><span class='label label-default'>"+item[moviesshows].movie.info+"</span></h4>"
-                                       + res_trailer
-                                 
-                                    +"</div></div>"
-                                 
-                                  +"</div><hr>");
-                             // if(item[moviesshows].movie.trailer.length)
-                          //        res_trailer = " - <a href='"+item[moviesshows].movie.trailer+"' target='_blank'>Trailer</a>";
-                          //  $("#movies").append(
-                          //              "<div class='item' data-id='' style='height:170px;'>"+
-                          //              "<img src='"+item[moviesshows].movie.poster+"' style='height:158px;width:100px;float:left;margin:0 5px 0 0;'>"+
-                          //              "<span class=''> "+item[moviesshows].movie.name+ "<br />"
-                          //                  +item[moviesshows].movie.info+"<br />"+
-                          //                  item[moviesshows].movie.time+
-                          //                 res_trailer+"</span>"+
-                                       
-                         //              "</div>");  
-                         //      res_trailer = "";
-                         res_trailer = "";
-                          a++;
-                            }
-                           
-                            count++;
-                            
-                      }
-                       
-                      
-                   console.log(cinemas);
-                  
-                      
-                },
-                error:function(e){
-                     $("#loading").hide();
-                     elNoService.show();
+
+            chrome.storage.local.get('options', function(data) {
+
+                info = data.options;
+
+                if (!info.length) {
+
+
+                    return false;
                 }
-             });
-              });
 
-        
-            
-            
+                $(".page-header").hide();
+
+
+                $.ajax({
+                    // url: "http://movies.log.local/api/v1/movies/loures?apiKey=debugkey",
+                    url: "http://showtimes.tiagomestre.pt/api/v2/movies/info/favourites",
+                    data: {info: info},
+                    type: 'GET',
+                    dataType: 'json', // added data type
+                    beforeSend: function() {
+
+                        $("#loading").show();
+                    },
+                    success: function(res) {
+
+                        $("#divSelect").show();
+
+                        $("#loading").hide();
+                        var jsonResponse = res;
+
+
+                        var tmpl = $('#simple').html();
+                       
+                        //var source   = $("#entry-template").html();
+                        var template = Handlebars.compile(tmpl);
+                        
+                         result = template(jsonResponse);
+                       
+                        $("#sessions").html(result);
+                       // $("#sessions").html(Mustache.render(tmpl, jsonResponse));
+                      
+                        $('#selectSala').bind('change', function() {
+
+                            var url = $(this).val(); // get selected value
+                           
+                            $('html,body').animate({scrollTop: $(url).offset().top}, 'slow');
+                        });
+
+                    },
+                    error: function(e) {
+                        $("#loading").hide();
+                        elNoService.show();
+                    }
+                });
+            });
+
+
+
+
         }
     };
-} (jQuery));
+}(jQuery));
 
 // ON DOM READY INIT APPLICATION.
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
 
     //THE APP MAIN MODULE CODE HAS ALREADY EXECUTED, SO WE CAN ACCESS THE INIT METHOD IMMEDIATELY.
     MS.Main.init();
-   
+
 });
